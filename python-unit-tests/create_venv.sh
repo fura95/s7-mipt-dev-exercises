41762cc9d620b30cc2076202d15ad5a5c387a48f
Vasily Baydin <v.baydin@s7.ru>
2021-03-01 09:30:25 +0300

    update venv creation

--- python-unit-tests/create_venv.sh
+++ python-unit-tests/create_venv.sh
@@ -1,6 +1,4 @@
-python3.8 -m venv venv 
-PYPI_PASSWORD=$(cat ~/.secrets/pypi_password.b64 | base64 -d )
+python3 -m venv venv 
 source ./venv/bin/activate
-./venv/bin/pip3 install --upgrade pip --index-url https://nexus.s7.aero/repository/nexus-pypi-proxy/simple
-./venv/bin/pip3 install  --index-url https://nexus.s7.aero/repository/nexus-pypi-proxy/simple -r ./requirements.txt
-./venv/bin/pip3 install  --index-url https://nexus.s7.aero/repository/nexus-pypi-proxy/simple -r ./host-requirements.txt
+./venv/bin/pip3 install --upgrade pip 
+./venv/bin/pip3 install  -r ./requirements.txt
